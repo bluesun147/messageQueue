fbaec26a1ec44d3544864bf26a07ecbacd4a2d28
bluesun147 <bluesun147@naver.com>
2024-07-13 23:44:39 +0900

    [Feat] 메시지 handle 구현

--- src/main/java/com/example/mq/handler/NotiMessageHandler.java
+++ src/main/java/com/example/mq/handler/NotiMessageHandler.java
@@ -1,6 +1,7 @@
 package com.example.mq.handler;
 
 
+import com.example.mq.NotiSender;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.logging.log4j.message.Message;
@@ -12,8 +13,17 @@ Component
 public class NotiMessageHandler implements MessageHandler {
     private final NotiSender notiSender;
 
+    public NotiMessageHandler(NotiSender notiSender) {
+        this.notiSender = notiSender;
+    }
+
     @Override
     public void handle(final Message message) {
-        notiSender.send();
+        if (message.getMessageType() != MessageType.NOTI) {
+            return;
+        }
+        notiSender.send(message.getContent());
+
+        log.info("메시지 NotiMessageHandelr에 의해 처리");
     }
 }
